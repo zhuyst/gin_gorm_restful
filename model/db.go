43a11a234e04467e6c2612c8f98a2c96774cd7db
zhuyst <13553371651@139.com>
2018-06-06 17:58:15 +0800

    使用字符串拼接生成数据库连接字符串

--- model/db.go
+++ model/db.go
@@ -3,20 +3,41 @@ package model
 import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"github.com/jinzhu/gorm"
+	"fmt"
 )
 
+// 数据库实例
 var db *gorm.DB
+
+const (
+	SqlType = "mysql"       // 数据库类型
+	Username = "test"       // 数据库用户名
+	Password = "zhuyst"     // 数据库密码
+	Hostname = "localhost"  // 数据库主机名
+	Port = "3306"           // 数据库端口号
+	Database = "test"       // 数据库名
+)
 
 func init() {
 	var err error
-	db, err = gorm.Open("mysql", "test:zhuyst@tcp(localhost:3306)/test?parseTime=true")
+
+	// 与数据库建立连接
+	db, err = gorm.Open(SqlType, GetDbUrl())
 	if err != nil {
 		panic("数据库连接失败")
 	}
 
+	// 设置生成的数据表均为单数形式
 	db.SingularTable(true)
 }
 
+// 获取数据库实例
 func GetDB() *gorm.DB {
 	return db
+}
+
+// 获取GORM的连接URL
+func GetDbUrl() string {
+	return fmt.Sprint(Username,":",Password,"@tcp(",Hostname,
+		":",Port,")/",Database,"?parseTime=true")
 }
